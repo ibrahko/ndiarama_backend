2543ede74b8088dfe9a33646ad4db7d295c9ed56
ibrahko <wecodefrom@gmail.com>
2026-04-02 23:53:30 +0200

    fix: CORS headers and middleware order

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -9,7 +9,20 @@ ALLOWED_HOSTS = env.list(
 
 # CORS : à restreindre par env
 CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
+CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[
+    "https://ndiarama-front.vercel.app",
+])
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 # Sécurité
 SESSION_COOKIE_SECURE = True
